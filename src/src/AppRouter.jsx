f51536dd8456f1dc3ff672f78b9dfb7350df4753
iamjothees <iamjothees@gmail.com>
2025-06-26 21:59:07 +0530

    feat: add react-router and setup basic routing
    - Create Home screen component
    - Add AppRouter with basic route configuration
    - Remove unused App.css and clean up App.jsx
    - Add react-router dependency to package.json

---
+++ src/src/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import Home from "./screens/Home"
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
